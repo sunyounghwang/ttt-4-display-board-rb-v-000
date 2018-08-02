3d4ed0e04624e0f51c7f85b7dcef96404e09bfe4
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-08-02 19:07:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board(board = " ")
+def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[4]} | #{board[5]} | #{board[6]} "
